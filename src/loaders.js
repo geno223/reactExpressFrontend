ab8f1c47c58ce6099ccc1c0058a6e147b095c618
geno223 <eugenio.jimenez22397@gmail.com>
2024-02-03 12:53:47 -0500

    fronted complete

---
+++ src/loaders.js
@@ -0,0 +1,15 @@
+const URL = "https://express-react-lab-dic6.onrender.com"
+
+
+
+export const projectsLoader = async () => {
+    const response = await fetch(URL + "/projects")
+    const projects = await response.json()
+    return projects
+}
+
+export const aboutLoader = async () => {
+    const response = await fetch(URL + "/about")
+    const about = await response.json()
+    return about
+}
